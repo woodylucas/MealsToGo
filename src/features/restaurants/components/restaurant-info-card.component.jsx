cf91a4accc92a4ae3f1958437f0545661bb987f7
woodylucas <wlucas0311@gmail.com>
2023-10-11 18:18:29 -0400

    refactor: updated styles and put in an external file to clean up

--- src/features/restaurants/components/restaurant-info-card.component.jsx
+++ src/features/restaurants/components/restaurant-info-card.component.jsx
@@ -1,51 +1,22 @@
 import React from "react";
-import { Image } from "react-native";
-import styled from "styled-components";
-import { Card } from "react-native-paper";
+
 import { SvgXml } from "react-native-svg";
+
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Address,
+  Info,
+  Rating,
+  Section,
+  SectionEnd,
+  Icon,
+} from "./restaurant-info-card.styles.js";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
 import star from "../../../../assets/star.js";
 import open from "../../../../assets/open.js";
-
-const RestaurantCard = styled(Card)`
-  background-color: ${({ theme }) => theme.colors.bg.primary};
-`;
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${({ theme }) => theme.space[3]};
-  background-color: ${({ theme }) => theme.colors.bg.primary};
-`;
-
-const Address = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.body};
-  font-size: ${({ theme }) => theme.fontSizes.caption};
-`;
-
-const Info = styled.View`
-  padding: ${({ theme }) => theme.space[3]};
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${({ theme }) => theme.space[2]};
-  padding-bottom: ${({ theme }) => theme.space[2]};
-`;
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-
-const Icon = styled.Image`
-  width: ${({ theme }) => theme.sizes[1]};
-  height: ${({ theme }) => theme.sizes[1]};
-`;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {

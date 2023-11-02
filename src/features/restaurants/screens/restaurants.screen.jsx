16ab7d5a9078172dfeb82bd6b6e824270e7de8e5
woodylucas <wlucas0311@gmail.com>
2023-11-02 16:58:47 -0400

    feat: added a loading indicator

--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -2,18 +2,26 @@ import { RestaurantInfoCard } from "../components/restaurant-info-card.component
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Searchbar } from "react-native-paper";
 import {
+  Loading,
+  LoadingContainer,
   RestaurantList,
   SearchContainer,
 } from "./restaurants.screen.styles.js";
 
 import { SafeArea } from "../../../components/utiliy/safe-area.component";
 import { useRestaurants } from "../../../utils/hooks/restaurantHooks";
+import { MD2Colors } from "react-native-paper";
 
 export const RestaurantsScreen = () => {
   const { restaurants, isLoading, error } = useRestaurants();
 
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={MD2Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar mode="view" />
       </SearchContainer>
--- src/features/restaurants/screens/restaurants.screen.styles.js
+++ src/features/restaurants/screens/restaurants.screen.styles.js
@@ -1,5 +1,7 @@
 import { FlatList } from "react-native";
 
+import { ActivityIndicator } from "react-native-paper";
+
 import styled from "styled-components/native";
 
 const RestaurantList = styled(FlatList).attrs({
@@ -12,4 +14,14 @@ const SearchContainer = styled.View`
   padding: ${({ theme }) => theme.space[3]};
 `;
 
-export { RestaurantList, SearchContainer };
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+export { RestaurantList, SearchContainer, LoadingContainer, Loading };
